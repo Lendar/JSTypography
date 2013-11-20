47decbe02b63783e3dd152dc30187da312b846fb
Lendar <xeldim@gmail.com>
2013-11-20 16:19:42 +0400

    add Universal Module Definition wrapper
    Copied from:
    
    https://github.com/umdjs/umd/blob/992cc0d071b7ebcd7feaad2a5349bdbeab09a0fe/returnExports.js#L40-L60

--- jstypography.js
+++ jstypography.js
@@ -9,8 +9,26 @@
  * @example   jstypograf({obj:["my_button","button2"],nbspobj:"sub_button",bind:"click",language:"rus",type:"norm",txts:["textarea1","bl17~textarea2","+abc"],off:["quotes"]})
  */
 
-(function(undefined){
-	window.typo=window.jstypograf=window.jstypograph=jstypo=function(TYPO){
+// if the module has no dependencies, the above pattern can be simplified to
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(this, function () {
+
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return function(TYPO){
 		/** Отключение функций **/
 		if(!Array.indexOf){	//IE must die
 			Array.prototype.indexOf = function(obj){
@@ -638,5 +656,4 @@
 			}
 		}
 	}
-}
-)();
+}));
